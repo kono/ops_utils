851df875f9bb15af41e64b2ac83d6b2d7bb08d78
Kono Hiroshi <hiroshi.kono@gmail.com>
2020-10-17 15:10:44 +0900

    Refactoring test script

--- test/ops_utils_test.rb
+++ test/ops_utils_test.rb
@@ -11,19 +11,20 @@ class OpsUtilsTest < Test::Unit::TestCase
     refute_nil ::OpsUtils::VERSION
   end
 
-  test 'has_updated_recently? method returns true with params "testfile" and 24 hours' do
-    o = OpsUtils_fake.new
-    stub(File).mtime('testfile'){Time.local(2020,10,16,10,0,0,0)}
-    stub(Time).now{Time.local(2020,10,17,0,0,0,0)}
+  sub_test_case 'assersions for has_updated_recently? method' do
+    setup do
+      stub(File).mtime('testfile'){Time.local(2020,10,16,10,0,0,0)}
+      stub(Time).now{Time.local(2020,10,17,0,0,0,0)}
+    end
 
-    assert_true o.has_updated_recently?('testfile', 24)
-  end
+    test 'has_updated_recently? method returns true with params "testfile" and 24 hours' do
+      o = OpsUtils_fake.new
+      assert_true o.has_updated_recently?('testfile', 24)
+    end
 
-  test 'has_updated_recentry? method returns false with params "testfile" and 12 hours' do
-    o = OpsUtils_fake.new
-    stub(File).mtime('testfile'){Time.local(2020,10,16,10,0,0,0)}
-    stub(Time).now{Time.local(2020,10,17,0,0,0,0)}
-
-    assert_false o.has_updated_recently?('testfile', 12)
+    test 'has_updated_recentry? method returns false with params "testfile" and 12 hours' do
+      o = OpsUtils_fake.new
+      assert_false o.has_updated_recently?('testfile', 12)
+    end
   end
 end
