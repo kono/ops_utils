d8431ae34e25b18bc3d3245410c8f84338c22194
Kono Hiroshi <hiroshi.kono@gmail.com>
2020-10-18 08:01:32 +0900

    Add exception to file non-existence when checking file timestamps

--- lib/ops_utils.rb
+++ lib/ops_utils.rb
@@ -3,11 +3,20 @@ require "ops_utils/version"
 module OpsUtils
   class Error < StandardError; end
 
+
   module_function
+
+  def get_file_mtime(file)
+    begin
+      File.mtime(file)
+    rescue => exception
+      Time.local(1,1,1)
+    end
+  end
   # fileの最終更新日時とNowの差がinterval_hoursと同じか小さければtrue, 
   # そうでなければfalseを返す
   def has_updated_recently?(file, interval_hours)
     interval_seconds = interval_hours * 3600
-    Time.now - File.mtime(file) < interval_seconds ? true : false
+    Time.now - get_file_mtime(file) < interval_seconds ? true : false
   end
 end
