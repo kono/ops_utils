eb5abbb1d046f6ba66c30effdb7ca6c29b25bc30
Kono Hiroshi <hiroshi.kono@gmail.com>
2020-10-18 09:12:10 +0900

    Enable to use wildcard for the argument of has_updated_recently? method

--- lib/ops_utils.rb
+++ lib/ops_utils.rb
@@ -3,18 +3,24 @@ require "ops_utils/version"
 module OpsUtils
   class Error < StandardError; end
 
-
   module_function
 
-  def get_file_mtime(file)
-    begin
-      File.mtime(file)
-    rescue => exception
+  def get_file_mtime(files)
+    # filesの最終更新日時を調べ、一番小さいものを返す。
+    # filesが非存在の場合、最終更新日時を西暦1年1月1日0:00にする
+    time_ar=[]
+    Dir.glob(files).each do |file|
+      time_ar << File.mtime(file)
+    end
+    if time_ar.length == 0
       Time.local(1,1,1)
+    else
+      time_ar.sort[0]
     end
   end
   # fileの最終更新日時とNowの差がinterval_hoursと同じか小さければtrue, 
-  # そうでなければfalseを返す
+  # そうでなければfalseを返す。
+  # fileはワイルドカード指定可能。その場合は最終更新日時が一番小さいものと比較する
   def has_updated_recently?(file, interval_hours)
     interval_seconds = interval_hours * 3600
     Time.now - get_file_mtime(file) < interval_seconds ? true : false
--- test/ops_utils_test.rb
+++ test/ops_utils_test.rb
@@ -8,16 +8,24 @@ class OpsUtilsTest < Test::Unit::TestCase
 
   sub_test_case 'assersions for has_updated_recently? method' do
     setup do
+      stub(Dir).glob('testfile*'){['testfile']}
       stub(File).mtime('testfile'){Time.local(2020,10,16,10,0,0,0)}
       stub(Time).now{Time.local(2020,10,17,0,0,0,0)}
+
+      stub(Dir).glob('testfile_not_exist'){[]}
     end
 
-    test 'has_updated_recently? method returns true with params "testfile" and 24 hours' do
-      assert_true OpsUtils.has_updated_recently?('testfile', 24)
+    test 'has_updated_recently? method returns true with params "testfile*" and 24 hours' do
+      assert_true OpsUtils.has_updated_recently?('testfile*', 24)
     end
 
-    test 'has_updated_recentry? method returns false with params "testfile" and 12 hours' do
-      assert_false OpsUtils.has_updated_recently?('testfile', 12)
+    test 'has_updated_recentry? method returns false with params "testfile*" and 12 hours' do
+      assert_false OpsUtils.has_updated_recently?('testfile*', 12)
     end
+
+    test 'has_updated_recentry? method returns false with params "testfile_not_exist" and 0 hours' do
+      assert_false OpsUtils.has_updated_recently?('testfile_not_exist', 0)
+    end
+
   end
 end
